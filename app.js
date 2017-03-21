974583cf9b4ca9d6f66535dd90d61da14c5d0600
komal Arya <guptakomal87@gmail.com>
2017-03-21 13:53:52 +0530

    session 5 javascript assign 5.1

---
+++ app.js
@@ -0,0 +1,5 @@
+var name = "Komal Arya"; 
+var age  = 29; 
+var dob = "21 aug 1987";
+var pob = "Pune";
+console.log("Name: "+ name + "\nAge: "+ age +"\nDate of birth: " + dob + "\nPlace of birth: " + pob ); 
